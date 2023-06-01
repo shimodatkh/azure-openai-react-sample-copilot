5711cdde2562f1ec6ae274c425cee59983c21f65
shimodatkh <tech150220@gmail.com>
2023-06-01 16:57:42 +0900

    :zap: テキスト入力フォーム・出力表示を追加

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,12 @@ import './App.css';
 
 import Button from 'react-bootstrap/Button';
 
-/**
-* Renders information about the signed-in user or a button to retrieve data about the user
-*/
 const ProfileContent = () => {
     const { instance, accounts } = useMsal();
     const [graphData, setGraphData] = useState(null);
-    
+    const [inputText, setInputText] = useState('');
+    const [outputText, setOutputText] = useState('');
+
     function RequestProfileData() {
         // Silently acquires an access token which is then attached to a request for MS Graph data
         instance
@@ -29,43 +28,55 @@ const ProfileContent = () => {
                 callMsGraph(response.accessToken).then((response) => setGraphData(response));
             });
     }
-    
+
+    function handleInputChange(event) {
+        setInputText(event.target.value);
+    }
+
+    function handleButtonClick() {
+        setOutputText(inputText);
+    }
+
     return (
         <>
             <h5 className="card-title">Welcome {accounts[0].name}</h5>
-            <br/>
+            <br />
             {graphData ? (
                 <ProfileData graphData={graphData} />
             ) : (
-                <Button variant="secondary" onClick={RequestProfileData}>
-                    Request Profile Information
-                </Button>
+                <>
+                    <div>
+                        <input type="text" value={inputText} onChange={handleInputChange} />
+                        <Button variant="secondary" onClick={handleButtonClick}>
+                            Submit
+                        </Button>
+                    </div>
+                    <br />
+                    <div>
+                        <textarea value={outputText} readOnly />
+                    </div>
+                </>
             )}
         </>
     );
 };
 
-/**
-* If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
-*/
 const MainContent = () => {
     return (
         <div className="App">
             <AuthenticatedTemplate>
                 <ProfileContent />
             </AuthenticatedTemplate>
-    
+
             <UnauthenticatedTemplate>
                 <h5>
-                    <center>
-                        Please sign-in to see your profile information.
-                    </center>
+                    <center>Please sign-in to see your profile information.</center>
                 </h5>
             </UnauthenticatedTemplate>
         </div>
     );
 };
-    
+
 export default function App() {
     return (
         <PageLayout>
